6e372abca8c862182c7c77692847345670845cd2
Cristhian Almeida <cristhianmga@hotmail.com>
2022-10-14 12:56:07 -0300

    Ajuste Relatório

--- AprendendoNovaVersao/Negocio/RelatorioMontagem.cs
+++ AprendendoNovaVersao/Negocio/RelatorioMontagem.cs
@@ -15,18 +15,23 @@ namespace AprendendoNovaVersao.Negocio
         public ActionResult<Relatorio> MontarRelatorio(DateTime data)
         {
             Relatorio relatorio = new Relatorio();
-            relatorio.DataVenda = data;
             var lancamentos = _padraoBD.ObterTodos<Lancamento>().Where(lancamento => lancamento.Data.Date == data.Date);
+
             if(lancamentos.Count() == 0)
             {
                 throw new Exception();
             }
+
             foreach (Lancamento item in lancamentos)
             {
+
+                relatorio.Mensagem += string.Format("\n{0} R${1}", item.Descricao, item.Valor);
                 relatorio.SaldoDiario += item.Valor;
             }
 
-            relatorio.Mensagem = string.Format("O saldo do dia {0} é : {1}", relatorio.DataVenda.Date, relatorio.SaldoDiario);
+            relatorio.DataVenda = data;
+            relatorio.Mensagem += string.Format("\n--------------------");
+            relatorio.Mensagem += string.Format("\nO saldo diario: R${0}", relatorio.SaldoDiario);
 
             return relatorio;
         }
